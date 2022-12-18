0bc9d48d4a3e9b757bfc39ccbbacf117260f895a
Pedro Correa <pedro.correa@zigpay.com.br>
2022-12-18 13:21:43 -0300

    :construction: tarmak for bh40hsrgb

--- keyboards/bm40hsrgb/keymaps/tulkdan/keymap.c
+++ keyboards/bm40hsrgb/keymaps/tulkdan/keymap.c
@@ -56,19 +56,19 @@ void rgb_matrix_indicators_user(void) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty
  * ,--------------------------------------------------------------------------------------------.
- * | Esc       |  Q  |     W      |  E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc       |  Q  |     W      |  J  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |-----------+-----+------------+-----+------+------+------+------+------+------+------+------|
- * | Tab       |  A  |     S      |  D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Tab       |  A  |     S      |  D  |   F  |   G  |   H  |   N  |   E  |   L  |   ;  |Enter |
  * |-----------+-----+------------+-----+------+------+------+------+------+------+------+------|
- * | Shift/Caps|  Z  |     X      |  C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  "   |
+ * | Shift/Caps|  Z  |     X      |  C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |  "   |
  * |-----------+-----+------------+-----+------+------+------+------+------+------+------+------|
  * | Ctrl      | GUI | Copy/Paste | Alt |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `--------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_mit(
-    KC_ESC,           KC_Q,    KC_W,              KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,           KC_A,    KC_S,              KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    TD(TD_LSFT_CAPS), KC_Z,    KC_X,              KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
+    KC_ESC,           KC_Q,    KC_W,              KC_J,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,           KC_A,    KC_S,              KC_D,    KC_F,    KC_G,    KC_H,    KC_N,    KC_E,    KC_L,    KC_SCLN, KC_ENT,
+    TD(TD_LSFT_CAPS), KC_Z,    KC_X,              KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_QUOT,
     KC_LCTL,          KC_LGUI, TD(TD_COPY_PASTE), KC_LALT, LOWER,      KC_SPC,        RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
